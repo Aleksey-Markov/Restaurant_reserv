35828fe5b27a75cd6f8c3c3320b07dd2191db90e
Aleksey Markov <tte4enuxa@gmail.com>
2024-09-25 23:09:30 +0700

    add tests

--- tables/tests.py
+++ tables/tests.py
@@ -1,1 +1,11 @@
 from django.test import TestCase
+
+from tables.models import Table
+
+
+class TableTest(TestCase):
+
+    def test_table_update(self):
+        table = Table(1, False)
+        self.assertEqual(table.table_num, 1)
+        self.assertequal(table.is_reserved, False)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class UserTest(TestCase):
+
+    def test_user_create(self):
+        user = User('e@email.com', '8800', 'petya')
+        self.assertEqual(user.name, 'petya')
+        self.assertEqual(user.phone, '8800')
+        self.assertEqual(user.email, 'e@email.com')
+
